b9e7aeeffc1db0198d50ee78d8de10c7c7c0485f
Jerko Steiner <jerko.steiner@gmail.com>
2015-09-12 18:43:38 +0200

    Added pub functions

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,8 @@ use std::io::Read;
 use std::io;
 use std::io::Write;
 use std::path::{PathBuf};
-use std::process::exit;
-use std::vec::Vec;
 
-struct Brightness {
+pub struct Brightness {
     backend: String,
     max_brightness: i32,
 }
@@ -35,15 +33,13 @@ impl Brightness {
 
         match content.trim().parse::<i32>() {
             Ok(value) => Ok(value),
-            Err(err) => {
-                printerr!("Error parsing value from file '{}': {}",
-                         path.display(), err);
-                Ok(0)
+            Err(_) => {
+                Ok(-1)
             }
         }
     }
 
-    fn set_brightness(&self, mut value: i32) -> Result<bool, io::Error> {
+    pub fn set_brightness(&self, mut value: i32) -> Result<bool, io::Error> {
         let max = try!(self.get_max_brightness());
         if value > max {
             value = max;
@@ -65,25 +61,25 @@ impl Brightness {
         }
     }
 
-    fn get_max_brightness(&self) -> Result<i32, io::Error> {
+    pub fn get_max_brightness(&self) -> Result<i32, io::Error> {
         if self.max_brightness > 0 {
             return Ok(self.max_brightness);
         }
         return self.get("max_brightness");
     }
 
-    fn get_brightness(&self) -> Result<i32, io::Error> {
+    pub fn get_brightness(&self) -> Result<i32, io::Error> {
         return self.get("brightness");
     }
 
-    fn get_percent(&self) -> Result<i32, io::Error> {
+    pub fn get_percent(&self) -> Result<i32, io::Error> {
         let value = try!(self.get_brightness()) as f32;
         let max = try!(self.get_max_brightness()) as f32;
         let result = (100 as f32) * (value + 0.5) / max;
         return Ok(result as i32);
     }
 
-    fn set_percent(&self, value: i32) -> Result<bool, io::Error> {
+    pub fn set_percent(&self, value: i32) -> Result<bool, io::Error> {
         let max = try!(self.get_max_brightness());
         let value = (value as f32) / (100_f32) * (max as f32) + 0.5_f32;
         let value = value as i32;
